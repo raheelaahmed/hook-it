dec56b6e497e37576208e68b0bd51c1b3f504045
Raheela Aftab <raheela.aftab@gmail.com>
2025-01-24 12:13:06 +0000

    added quantity and pattern count functionality in context file

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,18 +1,29 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from patterns.models import Pattern
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    pattern_count = 0
+    bag = request.session.get('bag', {})
 
-   
+    for item_id, quantity in bag.items():
+        pattern = get_object_or_404(Pattern, pk=item_id)
+        total += quantity * pattern.price
+        pattern_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'pattern': pattern,
+        })
     
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'pattern_count': pattern_count,
 
     }
 
