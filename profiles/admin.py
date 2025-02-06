2be76809e9cf4bf9c39987fde23c833beecb2da9
Raheela Aftab <raheela.aftab@gmail.com>
2025-02-06 22:27:19 +0000

    added profile form, java file for country field and css for profile app

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_phone_number', 'default_country', 'default_town_or_city')  # Add other fields you want to display
+    search_fields = ['user__username', 'default_phone_number', 'default_country']  # You can also search by related fields
