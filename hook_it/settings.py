b1c7ec78edec03ee89c9c4407c12d72075b43fb1
Raheela Aftab <raheela.aftab@gmail.com>
2025-02-09 20:30:27 +0000

    deploying

--- hook_it/settings.py
+++ hook_it/settings.py
@@ -211,7 +211,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
@@ -239,7 +239,7 @@ if 'USE_AWS' in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+"""DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'"""
 
 # Stripe
 
