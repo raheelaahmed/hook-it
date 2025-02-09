f86e55ee584d7082210665e155927001ae93fc48
Raheela Aftab <raheela.aftab@gmail.com>
2025-02-09 20:36:02 +0000

    deployment

--- hook_it/settings.py
+++ hook_it/settings.py
@@ -239,7 +239,7 @@ if 'USE_AWS' in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-"""DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'"""
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 
