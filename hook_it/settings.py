e6e1583a0de79c5d486e0225e2ce3b9d7871ad00
Raheela Aftab <raheela.aftab@gmail.com>
2025-02-10 13:48:22 +0000

    added email settings

--- hook_it/settings.py
+++ hook_it/settings.py
@@ -31,9 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-"'DEVELOPMENT' in os.environ"
+DEBUG = True   
+""" 'DEVELOPMENT' in os.environ"""
 ALLOWED_HOSTS = ['8000-raheelaahmed-hookit-n3puhx18hco.ws.codeinstitute-ide.net',
                 '127.0.0.1', 'localhost', 'hook-it-85e92c23978c.herokuapp.com',
                 ]
@@ -128,7 +127,6 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -139,14 +137,7 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 
-# email
 
-EMAIL_HOST = 'smtp.gmail.com'  # Gmail's SMTP server
-EMAIL_PORT = 587  # Use 587 for TLS
-EMAIL_USE_TLS = True  # Use TLS (True/False)
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER ')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('SDEFAULT_FROM_EMAIL')
 
 
 WSGI_APPLICATION = 'hook_it.wsgi.application'
@@ -220,6 +211,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS setting
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'hook-it'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -247,3 +245,17 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
+
+
+# email
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'patattern.hookit@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
