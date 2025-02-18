de2f9716e0c992481abd8f0f7f32201bddbb7e85
Raheela Aftab <raheela.aftab@gmail.com>
2025-02-18 15:21:20 +0000

    added css for contact us and added delete review and its view and url

--- patterns/urls.py
+++ patterns/urls.py
@@ -8,10 +8,12 @@ urlpatterns = [
     path('pattern/<int:pattern_id>/', views.pattern_detail, name='pattern-detail'),
 
     # Add a review for a specific pattern
+    
     path('pattern/<int:pattern_id>/add-review/', views.add_review, name='add-review'),
     path('review/edit/<int:review_id>/',views.edit_review, name='edit-review'),
     path('add/', views.add_pattern, name='add_pattern'),
     path('edit/<int:pattern_id>/', views.edit_pattern, name='edit_pattern'),
     path('delete/<int:pattern_id>/', views.delete_pattern, name='delete_pattern'),
+    path('pattern/<int:pattern_id>/review/<int:review_id>/delete/', views.delete_review, name='delete-review'),
    
 ]
