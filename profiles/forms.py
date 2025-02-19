b1fde4e927be8a30573b4f82847551c5576574d5
Raheela Aftab <raheela.aftab@gmail.com>
2025-02-19 10:05:24 +0000

    added css for profile page and added more field in profile form

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,7 +5,10 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('default_postcode','default_town_or_city','default_street_address1','default_street_address2','default_county','default_country')
+        exclude = ('user',)
+
+        
+       
 
     def __init__(self, *args, **kwargs):
         """
@@ -17,20 +20,20 @@ class UserProfileForm(forms.ModelForm):
            
             'default_phone_number': 'Phone Number',
             'default_user_email':'email',
-            #'default_postcode': 'Postal Code',
-            #'default_town_or_city': 'Town or City',
-            #'default_street_address1': 'Street Address 1',
-            #'default_street_address2': 'Street Address 2',
-            #'default_county': 'County, State or Locality',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
            }
 
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
-        #for field in self.fields:
-         #  if field != 'default_country':
-           #     if self.fields[field].required:
-             #       placeholder = f'{placeholders[field]} *'
-             #  else:
-               #     placeholder = placeholders[field]
-               # self.fields[field].widget.attrs['placeholder'] = placeholder
-            #self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-           # self.fields[field].label = False
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                    self.fields[field].widget.attrs['placeholder'] = placeholder
+                    self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+                    self.fields[field].label = False
