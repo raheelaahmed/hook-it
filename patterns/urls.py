5dd06feaaab7859f74f059a0e253424086eded98
Raheela Aftab <raheela.aftab@gmail.com>
2025-03-01 09:58:45 +0000

    removed ckeditor css and javascript files from base template

--- patterns/urls.py
+++ patterns/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
       path('', views.all_patterns, name='patterns'),
-      path('pattern/<int:pattern_id>/', views.pattern_detail, name='pattern-detail'),
-      path('pattern/<int:pattern_id>/add-review/', views.add_review, name='add-review'),
+      path('/<int:pattern_id>/', views.pattern_detail, name='pattern-detail'),
+      path('/<int:pattern_id>/add-review/', views.add_review, name='add-review'),
       path('review/edit/<int:review_id>/', views.edit_review, name='edit-review'),
-      path('pattern/<int:pattern_id>/review/<int:review_id>/delete/', views.delete_review, name='delete-review'),
+      path('/<int:pattern_id>/review/<int:review_id>/delete/', views.delete_review, name='delete-review'),
       path('add/', views.add_pattern, name='add_pattern'),
       path('edit/<int:pattern_id>/', views.edit_pattern, name='edit_pattern'),
       path('delete_pattern/<int:pattern_id>/', views.delete_pattern, name='delete_pattern'),
